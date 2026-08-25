c557c3fc18131289ff5f54c1bc9f62bcc21843be
chavato17 <ayubancrisjohn@gmail.com>
2026-08-25 10:18:55 +0800

    usab

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ window.addEventListener("DOMContentLoaded", initTextRotator);
 
 
 
-function ff(){
+function funcfunc(){
     const viewport = document.querySelector('.slider-viewport');
     const container = document.getElementById('projectsContainer');
     const cards = container.querySelectorAll('.projects-card');
